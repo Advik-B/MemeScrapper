4537bebb79003efcf38ecfb425b3225c073eec01
Advik <advik.b@gmail.com>
2022-11-18 19:16:28 +0530

    Update lib_scrapper.py

--- lib_scrapper.py
+++ lib_scrapper.py
@@ -16,11 +16,10 @@ REDDIT_URLS_REGEXES = (
     r"https://v.redd.it/[^/]+",
     # Reddit.com
     r"https://www.reddit.com/r/[^/]+/comments/[^/]+/[^/]+",
-    # i.imgur.com
-    r"https://i.imgur.com/[^/]+",
 )
 
 REDDIT_COMMENTS_REGEX = r"https://www.reddit.com/r/[^/]+/*"
+IMGUR_REGEX = r"https://i.imgur.com/[^/]+"
 
 def load_urls_(filename):
     with open(filename) as f:
@@ -50,7 +49,6 @@ def remove_duplicates(urls: tuple):
 
 def get_image_urls_reddit(urls: tuple[str], even_reddit_comments: bool = False):
     reddit_regex = REDDIT_URLS_REGEXES[:-1]  # From the first element to the second-from-the-last element
-    reddit_comments_regex = REDDIT_URLS_REGEXES[0]
     valid_urls = []
     for url in urls:
         for regex in reddit_regex:
@@ -68,6 +66,14 @@ def get_image_urls_reddit(urls: tuple[str], even_reddit_comments: bool = False):
 
     return _final_urls
 
+def download(url: str):
+    r = get(url, stream=True)
+    filename_: str = url.split("/")[-1]
+    with open(pjoin(save_path, filename_), "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
 
 def get_image_from_reddit_comments(url: str, save: bool = False):
     time_to_wait = 5
@@ -89,6 +95,21 @@ def get_image_from_reddit_comments(url: str, save: bool = False):
         if re.match(_i_reddit, im):
             return im
 
+def get_images(urls: tuple[str], even_reddit_comments: bool = False):
+    # Sort the urls into reddit and non-reddit
+    reddit_urls = []
+    non_reddit_urls = []
+    for url in urls:
+        if re.match(REDDIT_URLS_REGEXES[0], url):
+            reddit_urls.append(url)
+        elif re.match(IMGUR_REGEX, url):
+            non_reddit_urls.append(url)
+
+    # Get the images from reddit
+    reddit_urls_ = get_image_urls_reddit(reddit_urls, even_reddit_comments=True)
+    # Get the images from non-reddit
+
+    
 
 def test():
     urls = load_urls_("memes.json")
