b688d19bf7bb9b977c59ba0f5a0bbfab2441c18d
Advik <advik.b@gmail.com>
2022-11-18 23:13:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import multiprocessing as mp
 from os import listdir
 from os.path import abspath
 from shutil import rmtree
+from tkinter import filedialog
 
 import psutil
-
 from lib_utility import (
     fs_check,
     process,
@@ -21,16 +21,41 @@ from lib_scrapper import (
     remove_duplicates,
 )
 
+
 def main():
-    urls = load_urls("data.json")
+    file_path = filedialog.askopenfilename(
+        title="Select the JSON file to parse",
+        filetypes=(("JSON files", "*.json"),)
+    )
+    if not file_path:
+        console.print("[red]No file selected. Exiting...[/]")
+        return
+    try:
+        urls = load_urls(file_path)
+    except KeyError as e:
+        console.print("[red]Invalid JSON file. Exiting...[/]")
+        return
     urls = remove_duplicates(urls)
+    for url in urls:
+        console.print(f"[green]Downloading[/] [yellow]{url}[/]")
+    scan_indepth = console.input("[i cyan]Do you want to deep scan the urls? [b white](may take a while)[/][/] [Y/N]: ")
+    if scan_indepth.lower() == "y":
+        console.print("[cyan]Deep scanning...[/]")
+        # TODO: Fix this asap
+        urls = get_image_urls(urls, even_reddit_comments=True)
+        console.print(urls, sep="\n")
+        # urls = remove_duplicates(urls)
+        console.print("[green]Deep scan complete.[/]")
+    else:
+        console.print("[green][b yellow]Skipping[/] deep scan.[/]")
+    console.print(f"[green]Found {len(urls)} urls.[/]")
     fs_check(SAVE_PATH)
     fs_check(OUTPUT_PATH)
     cpu_count = mp.cpu_count()
     # to run at a time
     per_process = max(len(urls) // cpu_count, 1)
     ram = psutil.virtual_memory().available
-    print(f"Available RAM: {ram // 1024 // 1024} MB")
+    console.print(f"Available RAM: {ram // 1024 // 1024} MB")
     # Divide the available ram by 3 to get the amount of ram
     # to use per process
     per_ram = ram // cpu_count
@@ -38,8 +63,7 @@ def main():
         raise MemoryError("Not enough (free) RAM to run this program!")
 
     per_process_ram = per_ram // per_process
-    print(f"Using {per_ram // 1024 // 1024} MB overall")
-    print(f"Using {per_process_ram // 1024 // 1024} MB per process")
+    console.print(f"Using {per_process_ram // 1024 // 1024} MB per process")
     with mp.Pool() as pool_1:
         pool_1.map(download, urls, chunksize=per_process_ram)
         pool_1.close()
