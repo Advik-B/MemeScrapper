67d30fff6fcf41d5341e930578038fcbc2151987
Advik <advik.b@gmail.com>
2022-11-18 18:32:22 +0530

    Update lib_scrapper.py

--- lib_scrapper.py
+++ lib_scrapper.py
@@ -1,20 +1,23 @@
 import json
 import re
 from requests import get
+from bs4 import BeautifulSoup
 
+# WARNING: Do NOT edit the regexes below or add any other regexes.
+# The functions HEAVILY depend on the order of the regexes.
+# Keep in mind.
 REDDIT_URLS_REGEXES = (
     r"https://www.reddit.com/r/[^/]+/comments/[^/]+/[^/]+/[^/]+",
-    # redd.it
-    r"https://redd.it/[^/]+",
     # i.redd.it
     r"https://i.redd.it/[^/]+",
-    # i.imgur.com
-    r"https://i.imgur.com/[^/]+",
     # Reddit videos
     r"https://v.redd.it/[^/]+",
     # Reddit.com
     r"https://www.reddit.com/r/[^/]+/comments/[^/]+/[^/]+",
+    # i.imgur.com
+    r"https://i.imgur.com/[^/]+",
 )
+
 
 def load_urls_(filename):
     with open(filename) as f:
@@ -30,24 +33,61 @@ def load_urls_(filename):
     urls = tuple(urls)
     return urls
 
+
 def url_filter(url: str):
     for regex in REDDIT_URLS_REGEXES:
         if re.match(regex, url):
             return True
     return False
 
+
 def remove_duplicates(urls: tuple):
     return tuple(set(urls))
 
+
+def get_image_urls(urls: tuple):
+    reddit_regex = REDDIT_URLS_REGEXES[:-2]  # From the first element to the second-from-the-last element
+    reddit_comments_regex = REDDIT_URLS_REGEXES[0]
+    valid_urls = []
+    for url in urls:
+        for regex in reddit_regex:
+            if re.match(regex, url):
+                valid_urls.append(url)
+                break
+
+    for url in valid_urls:
+        pass
+
+
+def get_image_from_reddit_comments(url: str):
+    time_to_wait = 5
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+    }
+    r = get(url, headers=headers, timeout=time_to_wait)
+    _i_reddit = REDDIT_URLS_REGEXES[1]
+    soup = BeautifulSoup(r.text, "html.parser")
+    del r
+    # Save the html for debugging
+    with open("reddit.html", "w") as f:
+        f.write(soup.prettify())
+    images = soup.find_all("a", {"target": "_blank"})
+    for image in images:
+        im = image["href"]
+        if re.match(_i_reddit, im):
+            yield im
+
+
 def test():
     urls = load_urls_("memes.json")
+    TEST_URL = "https://www.reddit.com/r/ProgrammerHumor/comments/yyakjv/everyone_go_home_turns_out_all_our_work_runs/"
     first_len = len(urls)
     urls = remove_duplicates(urls)
     urls = filter(url_filter, urls)
     urls = tuple(urls)
     filt_len = len(urls)
     print(f"Filtered {first_len - filt_len} urls out of {first_len} urls")
-
+    get_image_urls(urls)
 
 if __name__ == "__main__":
     test()
